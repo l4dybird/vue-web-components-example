aa3fd4d0f5abb29e588a996e3db93299972d433c
l4dybird <future.l4dy61rd@gmail.com>
2024-09-23 18:06:38 +0900

    :bug: Uncaught ReferenceError: process is not defined エラーを回避する

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    // NOTE: Vite で Build した際に発生する Uncaught ReferenceError: process is not defined エラーを回避するために追加。
+    //       https://github.com/orgs/vuejs/discussions/8322
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+  },
   build: {
     lib: {
       entry: ["./src/main.ts"],
