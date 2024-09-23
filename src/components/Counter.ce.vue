48067beb071aa3ff72d8cbc8c45613dd44f452d7
l4dybird <future.l4dy61rd@gmail.com>
2024-09-23 17:37:44 +0900

    :sparkles: Counter コンポーネントを追加する

---
+++ src/components/Counter.ce.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="count++">count is {{ count }}</button>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #797979;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+button:hover {
+  border-color: #646cff;
+}
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+</style>
