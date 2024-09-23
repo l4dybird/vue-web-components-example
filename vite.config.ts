7e37f3d0b888040bdf8ee30d8520bd30d4e1bf85
l4dybird <future.l4dy61rd@gmail.com>
2024-09-23 17:46:54 +0900

    :wrench: build 用の設定を追加する

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: ["./src/main.ts"],
+      name: "MyWebComponents",
+      fileName: (format) => `vue-web-components-example.${format}.js`
+    },
+  },
   plugins: [vue({
     customElement: true
   })],
